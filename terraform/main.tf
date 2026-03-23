9059be069830592a85b80e5ebd73571c64ffe3a9
Jackson Camacho <jackson.camacho.hernandez@gmail.com>
2026-03-22 20:14:45 -0500

    Add DNS support and final optimizations

--- terraform/main.tf
+++ terraform/main.tf
@@ -141,3 +141,13 @@ resource "aws_spot_instance_request" "server" {
 
   tags = { Name = "minecraft-server" }
 }
+
+# --- DNS (Optional) ---
+resource "aws_route53_record" "minecraft" {
+  count   = var.route53_zone_id != "" ? 1 : 0
+  zone_id = var.route53_zone_id
+  name    = var.domain_name
+  type    = "A"
+  ttl     = "300"
+  records = [aws_spot_instance_request.server.public_ip]
+}
