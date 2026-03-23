44f753b55dc3f89b599bd5cb4b9576617bde4370
Jackson Camacho <jackson.camacho.hernandez@gmail.com>
2026-03-22 20:23:32 -0500

    Final variables update

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,13 @@ variable "instance_type" {
   description = "EC2 instance type"
   default     = "t3.small"
 }
+
+variable "route53_zone_id" {
+  description = "Route53 Hosted Zone ID for your domain (bijadillo.com)"
+  default     = "" # Empty means DNS won't be created
+}
+
+variable "domain_name" {
+  description = "Domain name for the server"
+  default     = "mc.bijadillo.com"
+}
